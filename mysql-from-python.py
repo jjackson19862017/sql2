a9642a95fcde36e82779fa7bc8c1a7286e654cd8
Stephen Jackson <jjackson19862017@gmail.com>
2020-01-17 11:15:23 +0000

    Initial Commit
    Signed-off-by: Stephen Jackson <jjackson19862017@gmail.com>

---
+++ mysql-from-python.py
@@ -0,0 +1,21 @@
+import os
+import pymysql
+
+# Get username from Cloud9 Workspace
+# Modify this variable if runnin on another enviroment
+
+username = os.getenv('C9_USER')
+
+# Connect to Database
+connection = pymysql.connect(host='localhost',user=username,password='',db='Chinook')
+
+try:
+    # Run a Query
+    with connection.cursor() as cursor:
+        sql = "Select * From Artist;"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+finally:
+    # Close the connection, Regardless of wether the above was successful
+    connection.close()
